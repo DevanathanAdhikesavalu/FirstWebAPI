8bc6ebd2e65bf20efd56e81912f36eeccba71555
Devanathan Adhikesavalu <devanathan.adhi@gavstech.com>
2023-09-21 12:03:16 +0530

    First WebApi commit

---
+++ FirstWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/calculator/add?x=10&y=20
+        [HttpGet("calculator/add")]
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        [HttpGet("calculator/sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y + 1000;
+        }
+        // api/calculator/subtract?x=200&y=10
+        [HttpPost]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        // api/calculator/multiply?x=10&y=20
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/calculator/divide?x=20&y=10
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
