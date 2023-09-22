58081974f47ec7e23c673c9419ec0e6106d4d173
DevanathanAdhikesavalu <devanathan.adhi@gavstech.com>
2023-09-22 11:26:05 +0530

    included Repository and controller for Employee

---
+++ FirstWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using FirstWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private RepositoryEmployee _repositoryEmployee;
+        public EmployeeController(RepositoryEmployee repository)
+        {
+            _repositoryEmployee = repository;
+        }
+        [HttpGet]
+        // GET: EmployeeController
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = _repositoryEmployee.GetAllEmployees();
+            return employees;
+        }
+
+        [HttpGet("/GetEmployees")]
+        public IEnumerable<EmployeeViewModel> GetAllEmployees()
+        {
+            List<Employee> employee = _repositoryEmployee.GetAllEmployees();
+            var empList = 
+                (
+                from emp in employee
+                select new EmployeeViewModel()
+                {
+                    EmpId = emp.EmployeeId,
+                    FirstName = emp.FirstName,
+                    LastName = emp.LastName,
+                    BirthDate = emp.BirthDate,
+                    HireDate = emp.HireDate,
+                    Title = emp.Title,
+                    City = emp.City,
+                    ReportsTo = emp.ReportsTo
+                }
+                ).ToList();
+            return empList;
+        }
+
+        [HttpPost]
+        public Employee EmployeeDetails(int id)
+        {
+            //Customer customer = _repositoryCustomers.FindCustomerById(id);
+            //return View(customer);
+            Employee employees = _repositoryEmployee.FindEmployeeById(id);
+            return employees;
+        }
+        [HttpPost("/AddNewEmployee")]
+        public int  AddNewEmployee([FromBody]Employee employee) 
+        {
+            _repositoryEmployee.AddNewEmployee(employee);
+            return 1;
+        }
+        [HttpPut]
+        public Employee EditEmployee(int id , [FromBody] Employee updatedEmployee)
+        {
+            
+            updatedEmployee.EmployeeId = id; // Ensure the ID in the URL matches the EmployeeId
+
+            Employee savedEmployee = _repositoryEmployee.UpdateEmployee(updatedEmployee);
+            return savedEmployee;
+        }
+        [HttpDelete]
+        public int DeleteEmployee(int id)
+        {
+            _repositoryEmployee.DeleteEmployee(id);
+            return 1;
+        }
+    }
+}
+
---
+++ FirstWebAPI/Models/RepositoryEmployee.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebAPI.Models
+{
+    public class RepositoryEmployee
+    {
+        private NorthwindContext _context;
+        public RepositoryEmployee(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee FindEmployeeById(int id) 
+        {
+            Employee employee=_context.Employees.Find(id);
+            return employee;
+        }
+        public Employee UpdateEmployee(Employee updatedEmployee) 
+        {
+            _context.Employees.Update(updatedEmployee);
+           // Console.WriteLine(_context.Entry(updatedEmployee).State); 
+            _context.SaveChanges();
+            return updatedEmployee;
+        }
+        public void AddNewEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+        public int DeleteEmployee(int id)
+        {
+
+            var employee = _context.Employees.Find(id);
+
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+
+            return 1;
+
+        }
+    }
+}
